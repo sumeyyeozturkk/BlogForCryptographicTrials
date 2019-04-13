838a9ccb23caf5cb8e2ad3b8cb89c5845f5f4da4
Sumeyye Ozturk <sumeyyeozturk@icloud.com>
2019-04-14 00:59:10 +0300

    Created postview class

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
+from .models import *
 
-# Create your views here.
+
+class PostView(generic.ListView):
+	template_name = 'post_list.html'
+	context_object_name = 'posts'
+
+	def get_queryset(self):
+		return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
