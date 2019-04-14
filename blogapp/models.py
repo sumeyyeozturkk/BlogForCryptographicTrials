7c64ccf060fcad45f4a06847b1392e04fd57b8f5
Sumeyye Ozturk <sumeyyeozturk@icloud.com>
2019-04-14 11:25:58 +0300

    New model created for encrypted posts

--- blogapp/models.py
+++ blogapp/models.py
@@ -13,3 +13,14 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+
+class EncryptedPost(models.Model):
+    title = models.CharField(max_length=200)
+    plainText = models.TextField()
+    encryptedText= models.TextField(null=True)
+    decryptedText = models.TextField(null=True)
+    key = models.CharField(max_length=200,null=True)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
