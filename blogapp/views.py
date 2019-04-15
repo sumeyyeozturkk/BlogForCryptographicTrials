7bc76c84362588fba26d4cd2b688d978896b032b
Sumeyye Ozturk <sumeyyeozturk@icloud.com>
2019-04-15 11:56:03 +0300

    New view created for to encrpt text.

--- blogapp/views.py
+++ blogapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.utils import timezone
 from .models import *
+from . import aesEncrypt as aes
 
 
 class PostView(generic.ListView):
@@ -10,3 +11,19 @@ class PostView(generic.ListView):
 
 	def get_queryset(self):
 		return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+
+class EncryptedPostView(generic.ListView):
+	context_object_name = 'encryptedPosts'
+
+	def get_queryset(self):
+		encPost =EncryptedPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+		for i in range(0,len(encPost)):
+			key = str(encPost[i].key)
+			plainText = encPost[i].plainText
+			encText = aes.encrypt_text(key,plainText)
+			decText = aes.decrypt_text(key,encText)
+			encPost[i].encryptedText = encText
+			encPost[i].decryptedText = decText
+			encPost[i].save()
+		return encPost
